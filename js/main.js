afc9064f59432978b7e58c4361a1d9cbdffcf73c
--list <edgarkmarmit@gmail.com>
2021-01-13 20:43:50 -0300

    comandos js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"perâ", cor:"amarela"}, {nome:"laranja",cor:"laranja"}];
+console.log(frutas);
+alert (frutas[0].cor);
+
+
+//var lista = ["maça","perâ","laranja"];
+//lista.push("uva");
+/*var fruta = {nome:"maçã",cor:"vermelha"};
+console.log(fruta);
+lista.pop();
+console.log (lista);*/
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+
+/*var nome= "Edgar Marmit";
+var N1= 3;
+var N2= 5;
+var frase= "Japão é o melhor time do mundo.";
+alert( nome + "tem" + idade + "anos");
+alert (idade+idade2);
+console.log (nome);
+console.log (idade+idade2);
+console.log (frase.replace("Japão","Brasil"));
+console.log (frase.toLowerCase());
+console.log (N1 *N2);*/
+//console.log (frase.toUpperCase());
+//alert (frase.replace ("Japão","Brasil"));
