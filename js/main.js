a11f86574eec07c2d2118f0e0e0407abfbacd903
--list <edgarkmarmit@gmail.com>
2021-01-28 22:48:51 -0300

    funções para manipular elementos de página

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por cliclar</b>";
+}
+
+function redirecionar(){
+    window.open("https://globollabs.academy/");
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar (elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("página carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
 /*function soma (n1,n2){
     return n1+n2;
 }
-*/
 
-
-function validaIdade(idade){
-
-if (idade>=18){
-    validar=true
-}
- else {
-    validar=false
-}
-return validar;
-}
-
-var idade = prompt ("Qual sua idade?")
+//var idade = prompt ("Qual sua idade?")
 console.log (validaIdade(idade));
 console.log (validar);
 
